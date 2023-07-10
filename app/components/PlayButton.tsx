31fc847c7a108afef27d08a8008ac9f71bbd8955
Sami Zakir Ahmed <54m1z4k@gmail.com>
2023-07-10 20:30:35 +0100

    Created play button

---
+++ app/components/PlayButton.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+import { PlayIcon } from "@heroicons/react/24/solid";
+import { useRouter } from "next/navigation";
+
+interface PlayButtonProps {
+  movieId: string;
+}
+
+const PlayButton: React.FC<PlayButtonProps> = ({ movieId }) => {
+  const router = useRouter();
+
+  return (
+    <button
+      onClick={() => router.push(`/watch/${movieId}`)}
+      className="flex flex-row items-center w-auto px-2 py-1 text-xs font-semibold transition bg-white rounded-md md:py-2 md:px-4 lg:text-lg hover:bg-neutral-300"
+    >
+      <PlayIcon className="w-4 mr-1 text-black md:w-7" />
+      Play
+    </button>
+  );
+};
+
+export default PlayButton;
