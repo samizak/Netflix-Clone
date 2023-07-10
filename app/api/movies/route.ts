c46deb2b5b086367a8d2c7c2e936c6ad87575f5b
Sami Zakir Ahmed <54m1z4k@gmail.com>
2023-07-10 18:38:45 +0100

    Added server authentication

--- app/api/movies/route.ts
+++ app/api/movies/route.ts
@@ -8,15 +8,8 @@ export async function GET(request: Request) {
     const session = await getServerSession(authOptions);
     if (!session) return NextResponse.json({ message: "You are not logged in." });
 
-    const movieCount = await prismadb.movie.count();
-    const randomIndex = Math.floor(Math.random() * movieCount);
-
-    const randomMovies = await prismadb.movie.findMany({
-      take: 1,
-      skip: randomIndex,
-    });
-
-    return NextResponse.json(randomMovies[0], { status: 200 });
+    const movies = await prismadb.movie.findMany();
+    return NextResponse.json(movies, { status: 200 });
   } catch (error) {
     console.error(error);
     // return res.status(400).end();
