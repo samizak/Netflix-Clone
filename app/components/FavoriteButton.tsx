afd9034e8647e44029d890f5ad3f7b303e91df11
Sami Zakir Ahmed <54m1z4k@gmail.com>
2023-07-10 19:29:49 +0100

    Implemented favourite button functionality

---
+++ app/components/FavoriteButton.tsx
@@ -0,0 +1,48 @@
+import axios from "axios";
+import React, { useCallback, useMemo } from "react";
+import { PlusIcon, CheckIcon } from "@heroicons/react/24/outline";
+
+import useCurrentUser from "@/hooks/useCurrentUser";
+import useFavorites from "@/hooks/useFavourites";
+
+interface FavoriteButtonProps {
+  movieId: string;
+}
+
+const FavoriteButton: React.FC<FavoriteButtonProps> = ({ movieId }) => {
+  const { mutate: mutateFavorites } = useFavorites();
+  const { data: currentUser, mutate } = useCurrentUser();
+
+  const isFavorite = useMemo(() => {
+    const list = currentUser?.favoriteIds || [];
+    return list.includes(movieId);
+  }, [currentUser, movieId]);
+
+  const toggleFavorites = useCallback(async () => {
+    let response;
+
+    if (isFavorite) response = await axios.delete("/api/favorite", { data: { movieId } });
+    else response = await axios.post("/api/favorite", { movieId });
+
+    const updatedFavoriteIds = response?.data?.favoriteIds;
+
+    mutate({
+      ...currentUser,
+      favoriteIds: updatedFavoriteIds,
+    });
+    mutateFavorites();
+  }, [movieId, isFavorite, currentUser, mutate, mutateFavorites]);
+
+  const Icon = isFavorite ? CheckIcon : PlusIcon;
+
+  return (
+    <div
+      onClick={toggleFavorites}
+      className="flex items-center justify-center w-6 h-6 transition border-2 border-white rounded-full cursor-pointer group/item lg:w-10 lg:h-10 hover:border-neutral-300"
+    >
+      <Icon className="w-4 text-white group-hover/item:text-neutral-300 lg:w-6" />
+    </div>
+  );
+};
+
+export default FavoriteButton;
